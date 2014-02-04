d52ed70cddef688e935b08a6bbf657fbef446adf
PJakcson <Marc313@gmx.net>
2014-02-04 18:10:20 +0100

    Update testGit.java

--- testGit/src/testGit.java
+++ testGit/src/testGit.java
@@ -1,4 +1,3 @@
-
 public class testGit {
 
 	/**
@@ -8,6 +7,8 @@ public class testGit {
 		// TODO Auto-generated method stub
 		System.out.println("asdf");
 		System.out.println("dasd ist ein test");
+		
+		System.out.println("und jetzt nochmal zurück");
 	}
 
 }
