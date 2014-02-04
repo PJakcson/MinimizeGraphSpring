155f19cef824872b8a643ecf78c9eb09080f933d
PJakcson <Marc313@gmx.net>
2014-02-04 17:09:06 +0100

    tset

--- testGit/src/testGit.java
+++ testGit/src/testGit.java
@@ -7,6 +7,7 @@ public class testGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("asdf");
+		System.out.println("dasd ist ein test");
 	}
 
 }
